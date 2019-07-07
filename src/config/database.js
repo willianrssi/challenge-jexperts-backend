29acca2c6caff600708ec4291daf6c6dc50a9d66
Willian <willianrssi@gmail.com>
2019-07-07 18:05:13 -0300

    script start criado

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/jexperts')
+
+const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost/jexperts'
+
+module.exports = mongoose.connect(url, { useMongoClient: true })
 
 mongoose.Error.messages.general.required = 'O campo "{PATH}" é obrigatório'
