cc690e01c66e2d38b8a9b543ba487a6215ed15fe
Willian <willianrssi@gmail.com>
2019-07-10 00:05:19 -0300

    Retirado o campo unique de email

--- src/api/usuario/usuario.js
+++ src/api/usuario/usuario.js
@@ -4,7 +4,7 @@ const mongoose = restful.mongoose
 const usuarioSchema = new mongoose.Schema({
   nome: { type: String, required: true },
   telefone: { type: String },
-  email: { type: String, unique: true, required: true, dropDups: true },
+  email: { type: String, required: true },
   login: { type: String, required: true, unique: true, dropDups: true },
   senha: { type: String, required: true }
 })
